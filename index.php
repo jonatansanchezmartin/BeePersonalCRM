082b57fa9b127249022b173de8298be3c722c1f2
Jonatan Sanchez Martin <sanchez.martin.jonatan@gmail.com>
2024-04-22 20:28:10 +0200

    commit

--- index.php
+++ index.php
@@ -37,8 +37,125 @@
 
 
 <body>
+
+<div class="container">
+            
+        <nav>
+
+            <div class="logo">
+                <a href="#">
+                    <img 
+                        src="media/abejaLogo.png" 
+                        alt="Bee Personal CRM"
+                        class="abejaLogo" 
+                        >
+                </a>
+
+                <p>
+                    <b>
+                        Bee Personal CRM
+                    </b>
+                </p>
+
+            </div>
+
+            <ul class="menu">
+
+                <li>
+                    <a href="#"><b>Inicio</b></a>
+                </li>
+
+                <li>
+                    <a href="#"><b>Ingresar</b></a>
+                </li>
+
+                <li>
+                    <button class="botonAmarillo"><a href="#" class="enlaceBoton"><b>Registrarse</b></a></button>
+                </li>
+
+            </ul>
+
+
+
+        </nav>
+        
+
+        <header>
+
+            <div class="Fila1Columna1">
+
+                <h1>
+                    No te olvides de los tuyos y sus cumpleaños
+                </h1>
+
+                <p id="pfila1">
+                    CRM personal para organizar la información de tus contactos, crear recordatorios de cumpleaños y mantener el contacto con tus seres queridos.
+                </p>
+
+                <button class="botonNegro">
+                    <a href="#"><b>Empezar</b></a>
+                </button>
+
+            </div>
+
+            <div class="Fila1Columna2">
+                <img src="media/llamada.jpg" id="hombreTelefono" alt="Hombre al teléfono">
+            </div>
+
+
+        </header>
+
+        <section class="fila2">
+
+        <div class="col1Fila2">
+                <img src="media/llamada3.jpg" id="llamadaJoven">
+        </div>
+
+        <div class="col2Fila2">
+
+            <h2>
+                Di adiós al olvido de felicitar los cumpleaños
+            </h2>
+
+            <p>
+                Ahórrate la decepción y las disculpas al olvidar los cumpleaños.
+            </p>
+
+            <p>
+                Recibe recordatorios para no perderte esas fechas importantes.
+            </p>
+
+        </div>
+
+        </section>
+
+        <section class="fila3">
+
+
+        <div class="col1Fila3">
+
+            <h2>
+                ¿Para qué un CRM personal?
+            </h2>
+
+            <p>
+            ¿Cuándo fue la última vez que hablaste con tu mejor amigo de la secundaria? ¿Felicitaste a tu hermana por su aniversario de boda? ¿Te acuerdas del nombre de los hijos de tus primos?
+            </p>
+
+            <p>
+                Para todo esto y más sirve un CRM personal.
+            </p>
+
+        </div>
+
+        <div class="col2Fila3">
+                <img src="media/videollamada.jpg" id="llamadaJoven">
+        </div>
+
+</section>
     
 </body>
+
 </html>
 
 
