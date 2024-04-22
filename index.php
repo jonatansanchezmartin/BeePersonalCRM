3f25bf54b5dffc7c329e3e13aad98af6783330c8
Jonatan Sanchez Martin <sanchez.martin.jonatan@gmail.com>
2024-04-22 11:54:53 +0200

    guardado

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+    <meta 
+        charset="UTF-8"
+        >
+
+    <meta 
+        name="viewport" 
+        content="width=device-width, initial-scale=1.0"
+        >
+
+    <title>
+        Inicio. Bee Personal CRM
+    </title>
+
+    <link 
+        rel="stylesheet" 
+        type="text/css" 
+        href="./styles.css"
+        />
+
+    <meta 
+            name= "Bee Personal CRM" 
+            content= "Remember your loved ones and their birthdays."
+        >
+
+    <link 
+        rel="icon" 
+        href="./media/bringTheBacon.png" 
+        type="image/x-icon" 
+        sizes="any"
+    >
+
+</head>
+
+
+<body>
+    
+</body>
+</html>
+
+
